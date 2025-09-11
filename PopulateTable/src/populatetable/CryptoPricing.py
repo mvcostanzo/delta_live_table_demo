e89950cd0d1c5e9b611d60c339fe14d4ae890e56
Michael Costanzo <7095348+mvcostanzo@users.noreply.github.com>
2025-09-11 07:51:06 -0400

    Update to data stream

--- PopulateTable/src/populatetable/CryptoPricing.py
+++ PopulateTable/src/populatetable/CryptoPricing.py
@@ -19,41 +19,60 @@ class CryptoPricing:
 
     @staticmethod
     def WriteLatestPrice(func):
-            @wraps(func)
-            def wrapper(self): 
-                r2_client = boto3.client(
-                    's3',
-                    region_name=self.AWSRegion,
-                    endpoint_url=self.EndpointURL,
-                    aws_access_key_id=self.AWSKey,
-                    aws_secret_access_key=self.AWSSecret,
-                    verify = False
-                )
-                buffer = BytesIO()
-                returnSet, unixTimestamp = func(self)
-                returnSet.write_parquet(buffer)
-                buffer.seek(0)
+        @wraps(func)
+        def wrapper(self): 
+            r2_client = boto3.client(
+                's3',
+                region_name=self.AWSRegion,
+                endpoint_url=self.EndpointURL,
+                aws_access_key_id=self.AWSKey,
+                aws_secret_access_key=self.AWSSecret,
+                verify = False
+            )
+            buffer = BytesIO()
+            returnSet, unixTimestamp = func(self)
+            returnSet.write_parquet(buffer)
+            buffer.seek(0)
 
-                r2_client.put_object(
-                     Bucket = self.DestinationBucket,
-                     Key = f'{unixTimestamp}.parquet',
-                     Body = buffer.getvalue()
-                )
-            return wrapper
+            r2_client.put_object(
+                    Bucket = self.DestinationBucket,
+                    Key = f'{unixTimestamp}.parquet',
+                    Body = buffer.getvalue()
+            )
+        return wrapper
             
-    
+    @staticmethod
+    def LocalParquet(func):
+        @wraps(func)
+        def wrapper(self):
+            returnSet, unixTimestamp = func(self)
+            returnSet.write_parquet(rf'./{unixTimestamp}.parquet')
+        return wrapper
+
+
     @WriteLatestPrice
     def GetLatestPrice(self):
         session = r.Session()
         quoteURL = self.PricingAPI
         response = session.get(quoteURL, verify=False)
         responseStream = StringIO(response.text)
-        pricingDF = pl.read_json(responseStream)
-        latestValue = pricingDF.select(pl.col("Data").struct.field("BTC-USD").struct.field("VALUE")).to_series()
-        latestUpdate = pricingDF.select(pl.col("Data").struct.field("BTC-USD").struct.field("VALUE_LAST_UPDATE_TS"))
-        latestUpdateTS = pl.from_epoch(latestUpdate["VALUE_LAST_UPDATE_TS"])
+        
+        BTCpricingDF = pl.read_json(responseStream).unnest('Data').unnest(['BTC-USD']).with_columns(pl.lit("BTC").alias("COIN"))
+        ETHpricingDF = pl.read_json(responseStream).unnest('Data').unnest(['ETH-USD']).with_columns(pl.lit("ETH").alias("COIN"))
+        XRPpricingDF = pl.read_json(responseStream).unnest('Data').unnest(['XRP-USD']).with_columns(pl.lit("XRP").alias("COIN"))
+
+        pricingDF = pl.concat([
+            BTCpricingDF.select([pl.col("COIN"), pl.col("VALUE"), pl.col("VALUE_LAST_UPDATE_TS")]), 
+            ETHpricingDF.select([pl.col("COIN"), pl.col("VALUE"), pl.col("VALUE_LAST_UPDATE_TS")]), 
+            XRPpricingDF.select([pl.col("COIN"), pl.col("VALUE"), pl.col("VALUE_LAST_UPDATE_TS")])], 
+            how='align'
+        )
         currentPrice = pl.DataFrame({
-            "price": latestValue,
-            "price_time": latestUpdateTS
+            "COIN_ID": [1,2,3],
+            "COIN": pricingDF.select(pl.col("COIN")).to_series(),
+            "COIN_NAME": ['BITCOIN', 'ETHERIUM', 'XRP LEDGER'],
+            "price": pricingDF.select(pl.col('VALUE')).to_series(),
+            "price_time": pl.from_epoch(pricingDF.select(pl.col("VALUE_LAST_UPDATE_TS")).to_series())
         })
-        return currentPrice, latestUpdate["VALUE_LAST_UPDATE_TS"].first()
+    
+        return currentPrice, pricingDF.select(pl.col("VALUE_LAST_UPDATE_TS")).to_series().first()
